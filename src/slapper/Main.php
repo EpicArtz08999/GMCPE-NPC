9687846166025e34f5c3b863eb06aa76a6ee6498
Driesboy <coenendries3@gmail.com>
2016-12-12 17:21:11 +0100

    Update Main.php

--- src/slapper/Main.php
+++ src/slapper/Main.php
@@ -713,4 +713,54 @@ class Main extends PluginBase implements Listener {
             }
         }
     }
+     public function onPlayerMove(PlayerMoveEvent $ev){
+        $player = $ev->getPlayer();
+        $from = $ev->getFrom();
+        $to = $ev->getTo();
+        if($from->distance($to) < 0.1){
+            return;
+        }
+        $maxDistance = "16";
+        foreach($player->getLevel()->getNearbyEntities($player->getBoundingBox()->grow($maxDistance, $maxDistance, $maxDistance), $player) as $e){
+            if($e instanceof Player){
+                continue;
+            }
+            if(substr($e->getSaveId(), 0, 7) !== "Slapper"){
+                continue;
+            }
+            if($e->getSaveId() === "SlapperFallingSand"){
+                continue;
+            }
+            $xdiff = $player->x - $e->x;
+            $zdiff = $player->z - $e->z;
+            $angle = atan2($zdiff, $xdiff);
+            $yaw = (($angle * 180) / M_PI) - 90;
+            $ydiff = $player->y - $e->y;
+            $v = new Vector2($e->x, $e->z);
+            $dist = $v->distance($player->x, $player->z);
+            $angle = atan2($dist, $ydiff);
+            $pitch = (($angle * 180) / M_PI) - 90;
+            if($e->getSaveId() === "SlapperHuman"){
+                $pk = new MovePlayerPacket();
+                $pk->eid = $e->getId();
+                $pk->x = $e->x;
+                $pk->y = $e->y + $e->getEyeHeight();
+                $pk->z = $e->z;
+                $pk->yaw = $yaw;
+                $pk->pitch = $pitch;
+                $pk->bodyYaw = $yaw;
+            } else {
+                $pk = new MoveEntityPacket();
+                $pk->eid = $e->getId();
+                $pk->x = $e->x;
+                $pk->y = $e->y + $e->offset;
+                $pk->z = $e->z;
+                $pk->yaw = $yaw;
+                $pk->headYaw = $yaw;
+                $pk->pitch = $pitch;
+            }
+            $player->dataPacket($pk);
+        }
+    }
+}
 }
